f275bf113295ad8bdf83c00e75aac4c1fb863e63
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-05-08 16:54:28 +0300

    add movie component using bootstrab table

--- src/components/movie/movie.jsx
+++ src/components/movie/movie.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const movie = () => {
-  return <div>movie</div>;
-};
+const movie = ({ movie }) => (
+  <tr>
+    <td>{movie.title}</td>
+    <td>{movie.genre.name}</td>
+    <td>{movie.numberInStock}</td>
+    <td>{movie.dailyRentalRate}</td>
+    <td>
+      <button type="button" class="btn btn-primary">
+        Delete
+      </button>
+    </td>
+  </tr>
+);
 
 export default movie;
